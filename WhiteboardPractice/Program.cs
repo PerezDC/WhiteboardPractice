5803796f71a5a17a16633d3482b94bb2163196a0
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-10-02 23:47:28 -0700

    Add project files.

---
+++ WhiteboardPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WhiteboardPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "this is a test string";
+
+            Console.WriteLine(practice.ReverseString(test));
+            Console.WriteLine(practice.CountCharacters(test));
+
+        }
+    }
+}
---
+++ WhiteboardPractice/practice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WhiteboardPractice
+{
+    class practice
+    {
+        public static string ReverseString(string inputString)
+        {
+            string tempString = "";
+
+            for (int i = inputString.Length; i > 0; i--)
+            {
+                tempString += inputString[i - 1];
+            }
+
+            return tempString;
+        }
+
+
+        public static int CountCharacters(string inputString)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (Convert.ToString(inputString[i]) == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+
+}
